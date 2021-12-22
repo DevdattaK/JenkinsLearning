a64941382ba25808bfd7d9789276a0cf72b03bbc
Devdatta <devdattakwd@yahoo.com>
2021-12-21 20:32:16 -0500

    corrected issues with Greeter

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,22 +1,16 @@
 package com.example;
 
-/**
- * This is a class.
- */
+/** This is a class. */
 public class Greeter {
 
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
-
-  }
+  /** This is a constructor. */
+  public Greeter() {}
 
   /**
    * @param someone
    * @return
    */
-  public String greet(String SomeOne) {
-    return String.format("Hello, %s!", SomeOne);
+  public final String greet(final String someone) {
+    return String.format("Hello, %s!", someone);
   }
 }
